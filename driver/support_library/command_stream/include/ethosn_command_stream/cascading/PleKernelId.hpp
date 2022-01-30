205ddde9f941368886299eb04bb1edf4ca8b577b
Efrat Hassid <efrat.hassid@arm.com>
2022-01-30 09:52:59 +0200

    Use X-MACRO for PleKernelId enum definition
    Use X-MACRO for easier string-to-enum and enum-to-string
    
    Signed-off-by: Efrat Hassid <efrat.hassid@arm.com>

--- driver/support_library/command_stream/include/ethosn_command_stream/cascading/PleKernelId.hpp
+++ driver/support_library/command_stream/include/ethosn_command_stream/cascading/PleKernelId.hpp
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <cstdint>
+#include <cstring>
 
 namespace ethosn
 {
@@ -14,103 +15,145 @@ namespace command_stream
 namespace cascading
 {
 
+/// This list uses X macro technique.
+/// See https://en.wikipedia.org/wiki/X_Macro for more info
+#define ETHOSN_PLE_KERNEL_ID_LIST                                                                                      \
+    X(NOT_FOUND)                                                                                                       \
+    X(ADDITION_16X16_1)                                                                                                \
+    X(ADDITION_16X16_1_S)                                                                                              \
+    X(ADDITION_RESCALE_16X16_1)                                                                                        \
+    X(ADDITION_RESCALE_16X16_1_S)                                                                                      \
+    X(AVGPOOL_3X3_1_1_UDMA_16X16_1)                                                                                    \
+    X(AVGPOOL_3X3_1_1_UDMA_16X16_1_S)                                                                                  \
+    X(INTERLEAVE_2X2_2_2_16X16_1)                                                                                      \
+    X(MAXPOOL_2X2_2_2_8X8_4)                                                                                           \
+    X(MAXPOOL_2X2_2_2_8X16_2)                                                                                          \
+    X(MAXPOOL_2X2_2_2_16X16_1)                                                                                         \
+    X(MAXPOOL_2X2_2_2_8X32_1)                                                                                          \
+    X(MAXPOOL_2X2_2_2_8X8_4_S)                                                                                         \
+    X(MAXPOOL_2X2_2_2_8X16_2_S)                                                                                        \
+    X(MAXPOOL_2X2_2_2_16X16_1_S)                                                                                       \
+    X(MAXPOOL_2X2_2_2_8X32_1_S)                                                                                        \
+    X(MAXPOOL_3X3_2_2_EVEN_8X8_4)                                                                                      \
+    X(MAXPOOL_3X3_2_2_EVEN_8X16_2)                                                                                     \
+    X(MAXPOOL_3X3_2_2_EVEN_8X32_1)                                                                                     \
+    X(MAXPOOL_3X3_2_2_EVEN_8X8_4_S)                                                                                    \
+    X(MAXPOOL_3X3_2_2_EVEN_8X16_2_S)                                                                                   \
+    X(MAXPOOL_3X3_2_2_EVEN_8X32_1_S)                                                                                   \
+    X(MAXPOOL_3X3_2_2_ODD_8X8_4)                                                                                       \
+    X(MAXPOOL_3X3_2_2_ODD_8X16_2)                                                                                      \
+    X(MAXPOOL_3X3_2_2_ODD_8X32_1)                                                                                      \
+    X(MAXPOOL_3X3_2_2_ODD_8X8_4_S)                                                                                     \
+    X(MAXPOOL_3X3_2_2_ODD_8X16_2_S)                                                                                    \
+    X(MAXPOOL_3X3_2_2_ODD_8X32_1_S)                                                                                    \
+    X(MEAN_XY_7X7_8X8_1)                                                                                               \
+    X(MEAN_XY_7X7_8X8_1_S)                                                                                             \
+    X(MEAN_XY_8X8_8X8_1)                                                                                               \
+    X(MEAN_XY_8X8_8X8_1_S)                                                                                             \
+    X(PASSTHROUGH_8X8_1)                                                                                               \
+    X(PASSTHROUGH_8X8_2)                                                                                               \
+    X(PASSTHROUGH_8X8_4)                                                                                               \
+    X(PASSTHROUGH_16X8_1)                                                                                              \
+    X(PASSTHROUGH_32X8_1)                                                                                              \
+    X(PASSTHROUGH_8X16_1)                                                                                              \
+    X(PASSTHROUGH_8X16_2)                                                                                              \
+    X(PASSTHROUGH_16X16_1)                                                                                             \
+    X(PASSTHROUGH_8X32_1)                                                                                              \
+    X(SIGMOID_8X8_1)                                                                                                   \
+    X(SIGMOID_8X8_2)                                                                                                   \
+    X(SIGMOID_8X8_4)                                                                                                   \
+    X(SIGMOID_16X8_1)                                                                                                  \
+    X(SIGMOID_32X8_1)                                                                                                  \
+    X(SIGMOID_8X16_1)                                                                                                  \
+    X(SIGMOID_8X16_2)                                                                                                  \
+    X(SIGMOID_16X16_1)                                                                                                 \
+    X(SIGMOID_8X32_1)                                                                                                  \
+    X(SIGMOID_8X8_1_S)                                                                                                 \
+    X(SIGMOID_8X8_2_S)                                                                                                 \
+    X(SIGMOID_8X8_4_S)                                                                                                 \
+    X(SIGMOID_16X8_1_S)                                                                                                \
+    X(SIGMOID_32X8_1_S)                                                                                                \
+    X(SIGMOID_8X16_1_S)                                                                                                \
+    X(SIGMOID_8X16_2_S)                                                                                                \
+    X(SIGMOID_16X16_1_S)                                                                                               \
+    X(SIGMOID_8X32_1_S)                                                                                                \
+    X(TRANSPOSE_XY_8X8_1)                                                                                              \
+    X(TRANSPOSE_XY_8X8_2)                                                                                              \
+    X(TRANSPOSE_XY_8X8_4)                                                                                              \
+    X(TRANSPOSE_XY_16X8_1)                                                                                             \
+    X(TRANSPOSE_XY_32X8_1)                                                                                             \
+    X(TRANSPOSE_XY_8X16_1)                                                                                             \
+    X(TRANSPOSE_XY_8X16_2)                                                                                             \
+    X(TRANSPOSE_XY_16X16_1)                                                                                            \
+    X(TRANSPOSE_XY_8X32_1)                                                                                             \
+    X(LEAKY_RELU_8X8_1)                                                                                                \
+    X(LEAKY_RELU_8X8_2)                                                                                                \
+    X(LEAKY_RELU_8X8_4)                                                                                                \
+    X(LEAKY_RELU_16X8_1)                                                                                               \
+    X(LEAKY_RELU_32X8_1)                                                                                               \
+    X(LEAKY_RELU_8X16_1)                                                                                               \
+    X(LEAKY_RELU_8X16_2)                                                                                               \
+    X(LEAKY_RELU_16X16_1)                                                                                              \
+    X(LEAKY_RELU_8X32_1)                                                                                               \
+    X(LEAKY_RELU_8X8_1_S)                                                                                              \
+    X(LEAKY_RELU_8X8_2_S)                                                                                              \
+    X(LEAKY_RELU_8X8_4_S)                                                                                              \
+    X(LEAKY_RELU_16X8_1_S)                                                                                             \
+    X(LEAKY_RELU_32X8_1_S)                                                                                             \
+    X(LEAKY_RELU_8X16_1_S)                                                                                             \
+    X(LEAKY_RELU_8X16_2_S)                                                                                             \
+    X(LEAKY_RELU_16X16_1_S)                                                                                            \
+    X(LEAKY_RELU_8X32_1_S)                                                                                             \
+    X(DOWNSAMPLE_2X2_8X8_2)                                                                                            \
+    X(DOWNSAMPLE_2X2_8X8_4)                                                                                            \
+    X(DOWNSAMPLE_2X2_16X8_1)                                                                                           \
+    X(DOWNSAMPLE_2X2_32X8_1)                                                                                           \
+    X(DOWNSAMPLE_2X2_8X16_1)                                                                                           \
+    X(DOWNSAMPLE_2X2_8X16_2)                                                                                           \
+    X(DOWNSAMPLE_2X2_16X16_1)                                                                                          \
+    X(DOWNSAMPLE_2X2_8X32_1)
+
+// define actual enum
 enum class PleKernelId : uint16_t
 {
-    NOT_FOUND,
-    ADDITION_16X16_1,
-    ADDITION_16X16_1_S,
-    ADDITION_RESCALE_16X16_1,
-    ADDITION_RESCALE_16X16_1_S,
-    AVGPOOL_3X3_1_1_UDMA_16X16_1,
-    AVGPOOL_3X3_1_1_UDMA_16X16_1_S,
-    INTERLEAVE_2X2_2_2_16X16_1,
-    MAXPOOL_2X2_2_2_8X8_4,
-    MAXPOOL_2X2_2_2_8X16_2,
-    MAXPOOL_2X2_2_2_16X16_1,
-    MAXPOOL_2X2_2_2_8X32_1,
-    MAXPOOL_2X2_2_2_8X8_4_S,
-    MAXPOOL_2X2_2_2_8X16_2_S,
-    MAXPOOL_2X2_2_2_16X16_1_S,
-    MAXPOOL_2X2_2_2_8X32_1_S,
-    MAXPOOL_3X3_2_2_EVEN_8X8_4,
-    MAXPOOL_3X3_2_2_EVEN_8X16_2,
-    MAXPOOL_3X3_2_2_EVEN_8X32_1,
-    MAXPOOL_3X3_2_2_EVEN_8X8_4_S,
-    MAXPOOL_3X3_2_2_EVEN_8X16_2_S,
-    MAXPOOL_3X3_2_2_EVEN_8X32_1_S,
-    MAXPOOL_3X3_2_2_ODD_8X8_4,
-    MAXPOOL_3X3_2_2_ODD_8X16_2,
-    MAXPOOL_3X3_2_2_ODD_8X32_1,
-    MAXPOOL_3X3_2_2_ODD_8X8_4_S,
-    MAXPOOL_3X3_2_2_ODD_8X16_2_S,
-    MAXPOOL_3X3_2_2_ODD_8X32_1_S,
-    MEAN_XY_7X7_8X8_1,
-    MEAN_XY_7X7_8X8_1_S,
-    MEAN_XY_8X8_8X8_1,
-    MEAN_XY_8X8_8X8_1_S,
-    PASSTHROUGH_8X8_1,
-    PASSTHROUGH_8X8_2,
-    PASSTHROUGH_8X8_4,
-    PASSTHROUGH_16X8_1,
-    PASSTHROUGH_32X8_1,
-    PASSTHROUGH_8X16_1,
-    PASSTHROUGH_8X16_2,
-    PASSTHROUGH_16X16_1,
-    PASSTHROUGH_8X32_1,
-    SIGMOID_8X8_1,
-    SIGMOID_8X8_2,
-    SIGMOID_8X8_4,
-    SIGMOID_16X8_1,
-    SIGMOID_32X8_1,
-    SIGMOID_8X16_1,
-    SIGMOID_8X16_2,
-    SIGMOID_16X16_1,
-    SIGMOID_8X32_1,
-    SIGMOID_8X8_1_S,
-    SIGMOID_8X8_2_S,
-    SIGMOID_8X8_4_S,
-    SIGMOID_16X8_1_S,
-    SIGMOID_32X8_1_S,
-    SIGMOID_8X16_1_S,
-    SIGMOID_8X16_2_S,
-    SIGMOID_16X16_1_S,
-    SIGMOID_8X32_1_S,
-    TRANSPOSE_XY_8X8_1,
-    TRANSPOSE_XY_8X8_2,
-    TRANSPOSE_XY_8X8_4,
-    TRANSPOSE_XY_16X8_1,
-    TRANSPOSE_XY_32X8_1,
-    TRANSPOSE_XY_8X16_1,
-    TRANSPOSE_XY_8X16_2,
-    TRANSPOSE_XY_16X16_1,
-    TRANSPOSE_XY_8X32_1,
-    LEAKY_RELU_8X8_1,
-    LEAKY_RELU_8X8_2,
-    LEAKY_RELU_8X8_4,
-    LEAKY_RELU_16X8_1,
-    LEAKY_RELU_32X8_1,
-    LEAKY_RELU_8X16_1,
-    LEAKY_RELU_8X16_2,
-    LEAKY_RELU_16X16_1,
-    LEAKY_RELU_8X32_1,
-    LEAKY_RELU_8X8_1_S,
-    LEAKY_RELU_8X8_2_S,
-    LEAKY_RELU_8X8_4_S,
-    LEAKY_RELU_16X8_1_S,
-    LEAKY_RELU_32X8_1_S,
-    LEAKY_RELU_8X16_1_S,
-    LEAKY_RELU_8X16_2_S,
-    LEAKY_RELU_16X16_1_S,
-    LEAKY_RELU_8X32_1_S,
-    DOWNSAMPLE_2X2_8X8_2,
-    DOWNSAMPLE_2X2_8X8_4,
-    DOWNSAMPLE_2X2_16X8_1,
-    DOWNSAMPLE_2X2_32X8_1,
-    DOWNSAMPLE_2X2_8X16_1,
-    DOWNSAMPLE_2X2_8X16_2,
-    DOWNSAMPLE_2X2_16X16_1,
-    DOWNSAMPLE_2X2_8X32_1,
+#define X(a) a,
+    ETHOSN_PLE_KERNEL_ID_LIST
+#undef X
 };
+
+namespace ple_id_detail
+{
+static constexpr const char* g_PleKernelNames[] = {
+#define X(a) #a,
+    ETHOSN_PLE_KERNEL_ID_LIST
+#undef X
+};
+static constexpr uint16_t g_PleKernelNamesSize = sizeof(g_PleKernelNames) / sizeof(g_PleKernelNames[0]);
+}    // namespace ple_id_detail
+
+inline PleKernelId String2PleKernelId(const char* const str)
+{
+    for (uint16_t i = 0; i < ple_id_detail::g_PleKernelNamesSize; ++i)
+    {
+        if (strcmp(str, ple_id_detail::g_PleKernelNames[i]) == 0)
+        {
+            return static_cast<PleKernelId>(i);
+        }
+    }
+    return PleKernelId::NOT_FOUND;
+}
+
+inline const char* PleKernelId2String(const PleKernelId id)
+{
+    const auto idU32 = static_cast<uint32_t>(id);
+
+    if (idU32 >= ple_id_detail::g_PleKernelNamesSize)
+    {
+        return "NOT_FOUND";
+    }
+
+    return ple_id_detail::g_PleKernelNames[idU32];
+}
 
 }    // namespace cascading
 }    // namespace command_stream
