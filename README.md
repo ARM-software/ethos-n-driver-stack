23734838afd23867a6fba3e1b799a7ecfa0e3b18
Matthew Sloyan <matthew.sloyan@arm.com>
2022-02-08 11:54:55 +0000

    Update to the latest Arm NN
    Update EthosNMemCopyTests to use new MockWorkloadFactory.
    
    Signed-off-by: Matthew Sloyan <matthew.sloyan@arm.com>

--- README.md
+++ README.md
@@ -152,7 +152,7 @@ mkdir driver_stack
 cd driver_stack
 git clone https://github.com/Arm-software/armnn --branch master
 cd armnn/
-git reset --hard f42f56870c6201a876f025a423eb5540d7438e83
+git reset --hard c42a987aa53d0fd842c34dee90abef5a9ff15fa4
 cd ..
 git clone https://github.com/Arm-software/ethos-n-driver-stack --branch 22.02dev1
 ```
--- armnn-ethos-n-backend/test/EthosNMemCopyTests.cpp
+++ armnn-ethos-n-backend/test/EthosNMemCopyTests.cpp
@@ -1,24 +1,46 @@
 //
-// Copyright © 2018-2021 Arm Limited.
+// Copyright © 2018-2022 Arm Limited.
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "EthosNWorkloadFactoryHelper.hpp"
-
+#include <EthosNBackend.hpp>
 #include <EthosNWorkloadFactory.hpp>
-#include <aclCommon/test/MemCopyTestImpl.hpp>
+
+#include <armnnTestUtils/LayerTestResult.hpp>
+#include <armnnTestUtils/MemCopyTestImpl.hpp>
+#include <armnnTestUtils/MockBackend.hpp>
+
 #include <doctest/doctest.h>
-#include <reference/RefWorkloadFactory.hpp>
-#include <reference/test/RefWorkloadFactoryHelper.hpp>
+
+namespace
+{
+
+template <>
+struct MemCopyTestHelper<armnn::EthosNWorkloadFactory>
+{
+    static armnn::IBackendInternal::IMemoryManagerSharedPtr GetMemoryManager()
+    {
+        armnn::EthosNBackend backend;
+        return backend.CreateMemoryManager();
+    }
+
+    static armnn::EthosNWorkloadFactory GetFactory(const armnn::IBackendInternal::IMemoryManagerSharedPtr&)
+    {
+        armnn::EthosNConfig config{};
+        return armnn::EthosNWorkloadFactory(config);
+    }
+};
+
+}    // namespace
 
 TEST_SUITE("EthosNMemCopy")
 {
 
     TEST_CASE("CopyBetweenCpuAndEthosN")
     {
-        // NOTE: Ethos-N  only supports QAsymmU8 data
+        // NOTE: Ethos-N only supports QAsymmU8 data
         LayerTestResult<uint8_t, 4> result =
-            MemCopyTest<armnn::RefWorkloadFactory, armnn::EthosNWorkloadFactory, armnn::DataType::QAsymmU8>(false);
+            MemCopyTest<armnn::MockWorkloadFactory, armnn::EthosNWorkloadFactory, armnn::DataType::QAsymmU8>(false);
         auto predResult =
             CompareTensors(result.m_ActualData, result.m_ExpectedData, result.m_ActualShape, result.m_ExpectedShape);
         CHECK_MESSAGE(predResult.m_Result, predResult.m_Message.str());
@@ -27,7 +49,7 @@ TEST_SUITE("EthosNMemCopy")
     TEST_CASE("CopyBetweenEthosNAndCpu")
     {
         LayerTestResult<uint8_t, 4> result =
-            MemCopyTest<armnn::EthosNWorkloadFactory, armnn::RefWorkloadFactory, armnn::DataType::QAsymmU8>(false);
+            MemCopyTest<armnn::EthosNWorkloadFactory, armnn::MockWorkloadFactory, armnn::DataType::QAsymmU8>(false);
         auto predResult =
             CompareTensors(result.m_ActualData, result.m_ExpectedData, result.m_ActualShape, result.m_ExpectedShape);
         CHECK_MESSAGE(predResult.m_Result, predResult.m_Message.str());
@@ -36,7 +58,7 @@ TEST_SUITE("EthosNMemCopy")
     TEST_CASE("CopyBetweenCpuAndEthosNWithSubtensors")
     {
         LayerTestResult<uint8_t, 4> result =
-            MemCopyTest<armnn::RefWorkloadFactory, armnn::EthosNWorkloadFactory, armnn::DataType::QAsymmU8>(true);
+            MemCopyTest<armnn::MockWorkloadFactory, armnn::EthosNWorkloadFactory, armnn::DataType::QAsymmU8>(true);
         auto predResult =
             CompareTensors(result.m_ActualData, result.m_ExpectedData, result.m_ActualShape, result.m_ExpectedShape);
         CHECK_MESSAGE(predResult.m_Result, predResult.m_Message.str());
@@ -45,7 +67,7 @@ TEST_SUITE("EthosNMemCopy")
     TEST_CASE("CopyBetweenEthosNAndCpuWithSubtensors")
     {
         LayerTestResult<uint8_t, 4> result =
-            MemCopyTest<armnn::EthosNWorkloadFactory, armnn::RefWorkloadFactory, armnn::DataType::QAsymmU8>(true);
+            MemCopyTest<armnn::EthosNWorkloadFactory, armnn::MockWorkloadFactory, armnn::DataType::QAsymmU8>(true);
         auto predResult =
             CompareTensors(result.m_ActualData, result.m_ExpectedData, result.m_ActualShape, result.m_ExpectedShape);
         CHECK_MESSAGE(predResult.m_Result, predResult.m_Message.str());
