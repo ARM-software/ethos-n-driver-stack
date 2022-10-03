495f971b43b8f9ff8663d0ad7416ef5bcff79934
Rob Hughes <robert.hughes@arm.com>
2022-10-03 17:41:50 +0100

    Account for wraparound in profiling command idx
    For larger command streams, the number of commands executed can be
    larger than the profiling field has space for (currently 10 bits => max
    value of 1023). This leads to profiling results being difficult to
    interpret because the command number appears to restart from zero.
    
    This patch adds a heuristic in the driver library when dumping profiling
    information so that wraparound is detected and the original command
    indices are restored.
    
    In order to do this, the driver library dumping code now extracts
    individual fields from the firmware profiling entries and writes those
    fields to the JSON, rather than writing the binary value directly. This
    also simplifies further processing of profiling dumps.
    
    Signed-off-by: Rob Hughes <robert.hughes@arm.com>

--- driver/driver_library/src/DumpProfiling.cpp
+++ driver/driver_library/src/DumpProfiling.cpp
@@ -9,6 +9,8 @@
 #include "Utils.hpp"
 
 #include <ethosn_utils/Enums.hpp>
+#define ETHOSN_ASSERT_MSG(cond, msg) assert(cond)
+#include <ethosn_utils/NumericCast.hpp>
 
 #include <ostream>
 #include <string>
@@ -47,7 +49,11 @@ void DumpProfilingData(const std::vector<ProfilingEntry>& profilingData, std::os
         return;
     }
     outStream << "[\n";
-    auto DumpEntry = [& o = outStream](const ProfilingEntry& entry) {
+
+    // Counter to keep track of the command indexes in profiling entries from the firmware. See fixCmdIdx().
+    uint32_t highestCmdIdx = 0;
+
+    auto DumpEntry = [& o = outStream, &highestCmdIdx](const ProfilingEntry& entry) {
         o << "\t{\n";
         o << "\t\t"
           << R"("time_stamp": )" << std::to_string(entry.m_Timestamp.time_since_epoch().count()) << ",\n";
@@ -58,105 +64,229 @@ void DumpProfilingData(const std::vector<ProfilingEntry>& profilingData, std::os
         o << "\t\t"
           << R"("metadata_category": )" << std::to_string(static_cast<uint64_t>(entry.m_MetadataCategory)) << ",\n";
         o << "\t\t"
-          << R"("metadata_value":)"
+          << R"("metadata_value": )" << std::to_string(entry.m_MetadataValue) << ",\n";
+        o << "\t\t"
+          << R"("metadata":)"
           << "\n";
         o << "\t\t{\n";
+
+        // Because the firmware has a limited number of bits to store counters such as the command index,
+        // command streams with a lot of commands can overflow this counter and so we attempt to detect
+        // this and recover the original command index when processing the entries from the firmware.
+        // We make the assumption that the cmd idxs that we observe generally increase over time, and so if
+        // the value suddenly jumps backwards, it's likely wrapped around. We also account for some "jitter"
+        // in the observed values, i.e. it won't monotonically increase.
+        auto fixCmdIdx = [&](auto x) {
+            constexpr uint32_t N = 1024;    // Value at which the cmd idx will wrap around
+            const uint32_t diff  = (x - highestCmdIdx) % N;
+            if (diff < N / 2)
+            {
+                // Assume the new cmd idx is ahead of the largest seen one
+                x             = highestCmdIdx + diff;
+                highestCmdIdx = x;
+            }
+            else
+            {
+                // Assume the new cmd idx is behind the largest seen one
+                x = highestCmdIdx - (N - diff);
+            }
+            return x;
+        };
+
+        DataUnion kernelEntry = {};
+        kernelEntry.m_Raw     = utils::NumericCast<EntryData>(entry.m_MetadataValue);
+
         switch (entry.m_MetadataCategory)
         {
             case ProfilingEntry::MetadataCategory::FirmwareWfe:
             {
                 o << "\t\t\t"
-                  << R"("firmware_wfe_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareWfe",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("type": )" << std::to_string(kernelEntry.m_WfeFields.m_Type) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareInference:
             {
+                if (entry.m_Type == ProfilingEntry::Type::TimelineEventStart)
+                {
+                    // Start of inference => reset the command counter as the next command we expect to see will be the zeroth.
+                    highestCmdIdx = 0;
+                }
+
                 o << "\t\t\t"
-                  << R"("firmware_inference_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareInference")"
+                  << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareCommand:
             {
                 o << "\t\t\t"
-                  << R"("firmware_command_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareCommand",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_CommandFields.m_CommandIdx))
+                  << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareDma:
             {
                 o << "\t\t\t"
-                  << R"("firmware_dma_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareDma",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_DmaFields.m_CommandIdx)) << ",\n";
+                o << "\t\t\t"
+                  << R"("dma_category": )" << std::to_string(kernelEntry.m_DmaFields.m_DmaCategory) << ",\n";
+                o << "\t\t\t"
+                  << R"("dma_hardware_id": )" << std::to_string(kernelEntry.m_DmaFields.m_DmaHardwareId) << ",\n";
+                o << "\t\t\t"
+                  << R"("stripe_idx": )" << std::to_string(kernelEntry.m_DmaFields.m_StripeIdx) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareTsu:
             {
                 o << "\t\t\t"
-                  << R"("firmware_tsu_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareTsu",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_TsuFields.m_CommandIdx)) << ",\n";
+                o << "\t\t\t"
+                  << R"("stripe_idx": )" << std::to_string(kernelEntry.m_TsuFields.m_StripeIdx) << ",\n";
+                o << "\t\t\t"
+                  << R"("bank_id": )" << std::to_string(kernelEntry.m_TsuFields.m_BankId) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareMceStripeSetup:
             {
                 o << "\t\t\t"
-                  << R"("firmware_mce_stripe_setup_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareMceStripeSetup",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_MceStripeSetupFields.m_CommandIdx))
+                  << ",\n";
+                o << "\t\t\t"
+                  << R"("stripe_idx": )" << std::to_string(kernelEntry.m_MceStripeSetupFields.m_StripeIdx) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwarePleStripeSetup:
             {
                 o << "\t\t\t"
-                  << R"("firmware_ple_stripe_setup_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwarePleStripeSetup",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_PleStripeSetupFields.m_CommandIdx))
+                  << ",\n";
+                o << "\t\t\t"
+                  << R"("stripe_idx": )" << std::to_string(kernelEntry.m_PleStripeSetupFields.m_StripeIdx) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareLabel:
             {
                 o << "\t\t\t"
-                  << R"("firmware_label_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareLabel",)"
+                  << "\n";
+                char buffer[sizeof(kernelEntry.m_LabelFields.m_Chars) + 1];
+                strncat(buffer, reinterpret_cast<const char*>(kernelEntry.m_LabelFields.m_Chars),
+                        sizeof(kernelEntry.m_LabelFields.m_Chars));
+                o << "\t\t\t"
+                  << R"("chars": )" << buffer << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareDmaSetup:
             {
                 o << "\t\t\t"
-                  << R"("firmware_dma_setup_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareDmaSetup",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_DmaStripeSetupFields.m_CommandIdx))
+                  << ",\n";
+                o << "\t\t\t"
+                  << R"("stripe_idx": )" << std::to_string(kernelEntry.m_DmaStripeSetupFields.m_StripeIdx) << ",\n";
+                o << "\t\t\t"
+                  << R"("dma_category": )" << std::to_string(kernelEntry.m_DmaStripeSetupFields.m_DmaCategory) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareGetCompleteCommand:
             {
                 o << "\t\t\t"
-                  << R"("firmware_get_complete_command_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareGetCompleteCommand",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )"
+                  << std::to_string(fixCmdIdx(kernelEntry.m_CompleteCommandsFields.m_CommandIdx)) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareScheduleNextCommand:
             {
                 o << "\t\t\t"
-                  << R"("firmware_schedule_next_command_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareScheduleNextCommand",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )"
+                  << std::to_string(fixCmdIdx(kernelEntry.m_ScheduleCommandsFields.m_CommandIdx)) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareTimeSync:
             {
                 o << "\t\t\t"
-                  << R"("firmware_time_sync_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareTimeSync",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("time_sync_data0": )"
+                  << std::to_string(static_cast<int>(kernelEntry.m_TimeSyncFields.m_TimeSyncData[0])) << ",\n";
+                o << "\t\t\t"
+                  << R"("time_sync_data1": )"
+                  << std::to_string(static_cast<int>(kernelEntry.m_TimeSyncFields.m_TimeSyncData[1])) << ",\n";
+                o << "\t\t\t"
+                  << R"("time_sync_data2": )"
+                  << std::to_string(static_cast<int>(kernelEntry.m_TimeSyncFields.m_TimeSyncData[2])) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareAgent:
             {
                 o << "\t\t\t"
-                  << R"("firmware_agent_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareAgent",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("type": )" << std::to_string(kernelEntry.m_AgentFields.m_Type) << ",\n";
+                o << "\t\t\t"
+                  << R"("idx": )" << std::to_string(kernelEntry.m_AgentFields.m_Idx) << ",\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_AgentFields.m_CommandIdx)) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareAgentStripe:
             {
                 o << "\t\t\t"
-                  << R"("firmware_agent_stripe_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareAgentStripe",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("agent_stripe_type": )" << std::to_string(kernelEntry.m_AgentStripeFields.m_AgentStripeType)
+                  << ",\n";
+                o << "\t\t\t"
+                  << R"("agent_stripe_idx": )" << std::to_string(kernelEntry.m_AgentStripeFields.m_AgentStripeIdx)
+                  << ",\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_AgentStripeFields.m_CommandIdx))
+                  << ",\n";
+                o << "\t\t\t"
+                  << R"("stripe_idx": )" << std::to_string(kernelEntry.m_AgentStripeFields.m_StripeIdx) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::InferenceLifetime:
             {
                 o << "\t\t\t"
-                  << R"("inference_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "InferenceLifetime")"
+                  << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::BufferLifetime:
             {
                 o << "\t\t\t"
-                  << R"("buffer_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "BufferLifetime")"
+                  << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::CounterValue:
@@ -168,13 +298,23 @@ void DumpProfilingData(const std::vector<ProfilingEntry>& profilingData, std::os
             case ProfilingEntry::MetadataCategory::FirmwarePle:
             {
                 o << "\t\t\t"
-                  << R"("firmware_ple_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwarePle",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_PleFields.m_CommandIdx)) << ",\n";
+                o << "\t\t\t"
+                  << R"("stripe_idx": )" << std::to_string(kernelEntry.m_PleFields.m_StripeIdx) << "\n";
                 break;
             }
             case ProfilingEntry::MetadataCategory::FirmwareUdma:
             {
                 o << "\t\t\t"
-                  << R"("firmware_udma_value": )" << std::to_string(entry.m_MetadataValue) << "\n";
+                  << R"("category": "FirmwareUdma",)"
+                  << "\n";
+                o << "\t\t\t"
+                  << R"("command_idx": )" << std::to_string(fixCmdIdx(kernelEntry.m_UdmaFields.m_CommandIdx)) << ",\n";
+                o << "\t\t\t"
+                  << R"("stripe_idx": )" << std::to_string(kernelEntry.m_UdmaFields.m_StripeIdx) << "\n";
                 break;
             }
             default:
