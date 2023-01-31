180dbafce416096e2584d0de9019631801d40dd1
Mikael Olsson <mikael.olsson@arm.com>
2023-01-31 12:49:32 +0100

    Move big firmware struct to header file
    The big firmware struct has been moved to the header file so it can be
    used by other components.
    
    Signed-off-by: Mikael Olsson <mikael.olsson@arm.com>

--- kernel-module/ethosn_device.c
+++ kernel-module/ethosn_device.c
@@ -1331,28 +1331,6 @@ int ethosn_send_stash_request(struct ethosn_core *core)
  * Firmware
  ****************************************************************************/
 
-/* Big FW binary structure. Must be kept in sync with firmware binary layout */
-struct ethosn_big_fw {
-	uint32_t fw_magic;
-	uint32_t fw_ver_major;
-	uint32_t fw_ver_minor;
-	uint32_t fw_ver_patch;
-	uint32_t arch_min;
-	uint32_t arch_max;
-	uint32_t offset;
-	uint32_t size;
-	uint32_t code_offset;
-	uint32_t code_size;
-	uint32_t ple_offset;
-	uint32_t ple_size;
-	uint32_t vector_table_offset;
-	uint32_t vector_table_size;
-	uint32_t unpriv_stack_offset;
-	uint32_t unpriv_stack_size;
-	uint32_t priv_stack_offset;
-	uint32_t priv_stack_size;
-} __packed;
-
 static bool validate_big_fw_header(struct ethosn_core *core,
 				   struct ethosn_big_fw *big_fw)
 {
--- kernel-module/ethosn_device.h
+++ kernel-module/ethosn_device.h
@@ -52,6 +52,28 @@ struct ethosn_inference_queue {
 	struct list_head inference_queue;
 };
 
+/* Big FW binary structure. Must be kept in sync with firmware binary layout */
+struct ethosn_big_fw {
+	uint32_t fw_magic;
+	uint32_t fw_ver_major;
+	uint32_t fw_ver_minor;
+	uint32_t fw_ver_patch;
+	uint32_t arch_min;
+	uint32_t arch_max;
+	uint32_t offset;
+	uint32_t size;
+	uint32_t code_offset;
+	uint32_t code_size;
+	uint32_t ple_offset;
+	uint32_t ple_size;
+	uint32_t vector_table_offset;
+	uint32_t vector_table_size;
+	uint32_t unpriv_stack_offset;
+	uint32_t unpriv_stack_size;
+	uint32_t priv_stack_offset;
+	uint32_t priv_stack_size;
+} __packed;
+
 /*
  * This enum contains different error condition that can be reported
  * by our driver.
